859c4e259387e3bb2eaaa1ca6d4d79339360634d
Manuel <Manuel@Manuel-VAIO>
2018-02-03 21:33:30 +0100

    test

--- src/main/java/com/wstest/wstest/WstestApplication.java
+++ src/main/java/com/wstest/wstest/WstestApplication.java
@@ -2,20 +2,12 @@ package com.wstest.wstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan("com.wstest.controller")
-@RestController
+@ComponentScan("com.wstest.controller")
 @SpringBootApplication
 public class WstestApplication {
     
-	
-	@RequestMapping("/")
-	public String index() {
-		return "Hola Mundo!!";
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(WstestApplication.class, args);
 	}
